97c9061a0860abbe69c39db4186c9d39f2ba44da
paola ramos <curso@git.com>
2021-03-05 19:07:38 -0500

    se actualizan archivos php para insercion

---
+++ notas.php
@@ -0,0 +1,28 @@
+<?php
+$notas = 0;
+
+echo "***** Boletin de Notas ***** \n";
+
+$array = [
+        'ciencias' => '2.5',
+        'sociales' => '5.0',
+        'matematicas'  => '4.5',
+        'ingles'   => '3.5',
+    ];
+     
+    extract($array);
+    echo "\n";
+     
+   echo " ciencias = $ciencias \n sociales = $sociales \n matematicas = $matematicas \n ingles = $ingles \n";
+     
+   $j = 0;
+ 
+    foreach($array as $elemento)
+    {
+        if($elemento < 3.5){
+        echo "\n";
+        echo "la nota perdida es ".$elemento;
+       }
+       ++$j;
+    }
+?>
---
+++ operaciones_basicas.php
@@ -0,0 +1,17 @@
+<?php
+      echo "los numeros introducidos para realizar operaciones son  
+      ". $num1 ." y ". $num2."\n";
+  
+      $suma = $num1 + $num2;
+      echo "El resultado de la suma es  ".$suma."\n";
+  
+      $resta = $num1 - $num2;
+      echo "El resultado de la resta es  ".$resta."\n";
+     
+      $multi = $num1 * $num2;
+      echo "El resultado de la multiplicación es  ".$multi."\n"; 
+      
+      $div = $num1 / $num2;
+      echo "El resultado de la división es   ".$div."\n";
+    }
+?>
