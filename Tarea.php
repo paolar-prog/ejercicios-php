5136e2f5db1cf04dac7e0f2a25efb3c64e2442ee
paola ramos <paolaramos09@gmail.com>
2022-03-23 17:01:28 -0500

    Actualizacion tarea arreglos

--- Tarea.php
+++ Tarea.php
@@ -23,13 +23,14 @@ $precios = [
     3200, 
     2900, 
 ];
-$numAleatorio = rand(5,15);
 $num=0;
 
 for($i=0; $i <= 6; $i++){
     $num++;
+    $numAleatorio = rand(1,15);
     echo  "<br>  ".$num." ) ".$insumos[$i]. ".  costo unitario  $".number_format($precios[$i], 0, ",", ".").
     ", Cantidad: ".$numAleatorio.", total: $".number_format($numAleatorio*$precios[$i], 0, ",", ".")."</br>";   
 }   
 
 ?>
+
