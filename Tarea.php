78d799573787c47c43c10ff873a4919ae471c024
paola ramos <paolaramos09@gmail.com>
2022-03-23 12:18:50 -0500

    Actualizacion tarea arreglos

--- Tarea.php
+++ Tarea.php
@@ -1,7 +1,10 @@
 <?php
-/*yramos arreglos que permitan mostrar la siguiente salida
+/*Autor:yramos
+Fecha:23/03/2022 
+Funcionalidad: Arreglos numericos que permiten mostrar la siguiente salida
 1) Arroz. costo unitario $1.500, Cantidad: 2, total: $3.000
 2) Aceite. costo unitario $2.000, Cantidad: 5, total: $10.000 */ 
+
 $insumos = [
     'Arroz', 
     'Aceite', 
