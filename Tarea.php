a0396f4532977a4ce34247b9e85b2e19bcc1a23c
paola ramos <paolaramos09@gmail.com>
2022-03-23 12:16:24 -0500

    Actualizacion tarea arreglos

--- Tarea.php
+++ Tarea.php
@@ -1,4 +1,7 @@
 <?php
+/*yramos arreglos que permitan mostrar la siguiente salida
+1) Arroz. costo unitario $1.500, Cantidad: 2, total: $3.000
+2) Aceite. costo unitario $2.000, Cantidad: 5, total: $10.000 */ 
 $insumos = [
     'Arroz', 
     'Aceite', 
@@ -9,20 +12,18 @@ $insumos = [
     'Areparina', 
 ];
 $precios = [
-    '1650', 
-    '5800', 
-    '3200', 
-    '2000', 
-    '6400', 
-    '3200', 
-    '2900', 
+    1650, 
+    5800, 
+    3200, 
+    2000, 
+    6400, 
+    3200, 
+    2900, 
 ];
 $numAleatorio = rand(5,15);
 $num=0;
 
 for($i=0; $i <= 6; $i++){
-    for ($j = 0; $j <=6; $j++) {
-    }
     $num++;
     echo  "<br>  ".$num." ) ".$insumos[$i]. ".  costo unitario  $".number_format($precios[$i], 0, ",", ".").
     ", Cantidad: ".$numAleatorio.", total: $".number_format($numAleatorio*$precios[$i], 0, ",", ".")."</br>";   
